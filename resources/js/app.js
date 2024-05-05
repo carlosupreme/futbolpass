ae0b4591dcc353f6b20167a7ea8c6fa18c71fbbd
Raul <raul.tecnm.ito@gmail.com>
2024-05-04 18:06:35 -0600

    Add QR in dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
-import './bootstrap';
+import "./bootstrap";
+
+import QRCode from "qrcode";
+window.QRCode = QRCode;
--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,3 +1,5 @@
+@vite(['resources/js/app.js'])
+
 <div
     class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
 
@@ -9,7 +11,27 @@
     <form method="POST" action="{{ route('logout') }}" x-data class="my-10">
         @csrf
 
-        <a href="{{ route('logout') }}" class="bg-gray-400 rounded-lg p-4" @click.prevent="$root.submit();">Cerrar
+        <a href="{{ route('logout') }}" class="bg-slate-300 rounded-lg p-4" @click.prevent="$root.submit();">Cerrar
             sesión </a>
     </form>
+
+    <div>
+        <h2 class="text-xl font-medium mb-2 text-center">Mi código QR</h2>
+        <div class="border shadow w-fit m-auto">
+            <img id="qr-img" />
+        </div>
+        <p id="qr-error"></p>
+    </div>
 </div>
+
+<script type="module">
+    const userEmail = {{Js::from(Auth::user()->email) }};
+    let qrDiv = document.getElementById("qr-img");
+
+    QRCode.toDataURL(userEmail, {version: 3, width: 300}).then((url) => {
+        qrDiv.src = url;
+    }).catch(err => {
+        let p = document.getElementById("qr-error");
+        p.textContent = "No se pudo generar tu código QR";
+    });
+</script>
