0947837ae6370d4e942ba1fa10187c5f86f1992b
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2024-06-25 10:42:03 -0600

    redesigning login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Villa 7</title>
+
+    <link rel="shortcut icon" href="{{asset("logo.png")}}" type="image/x-icon">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
