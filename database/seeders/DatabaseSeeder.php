d40c7d28043757996b1fdd5c877c90220b10e89c
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2024-06-25 12:36:49 -0600

    removin seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,16 +23,5 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@admin.com',
             'password' => Hash::make('password')
         ])->assignRole('admin');
-
-        // Creates "UEFA Champions League", "Premier League", "Liga MX"
-        $this->call(LeagueSeeder::class);
-
-        // Creates "Sub 21" and "Primera División" seasons in all leagues
-        $this->call(SeasonSeeder::class);
-
-        // Creates 8 well-know euro teams in all seasons
-        $this->call(TeamSeeder::class);
-        // Creates one game between two random teams for each season
-        // $this->call(GameSeeder::class);
     }
 }
